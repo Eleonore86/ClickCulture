9876d44968c5add67c567d35262a1db56efbd5d9
Rabah Khabzaoui <neuro@hotmail.fr>
2020-11-03 12:13:21 +0100

    navigation

---
+++ react-culture/src/Component/Navigation.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from "./Home"
+import FormLibraire from "./FormLibraire"
+import Historique from "./Historique"
+import DetailLibraire from "./DetailLibraire"
+// import Menu from './Menu';
+import Login from './Login';
+// import { ThemeContext } from '../Tools/ThemeContext';
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            // theme: {
+            //     color : 'black',
+            //     bgColor: 'white'
+            // }
+         }
+    }
+    // ChangeTheme = (type) => {
+    //     if (type == "dark") {
+    //         setTheme
+    //     }
+    // }
+    render() { 
+        return ( 
+            <BrowserRouter>
+                <div className="container-fluid">
+                    <Switch>
+                        <Route path="/" exact component={Home}></Route>
+                        <Route path="/addLibraire" component={FormLibraire}></Route>
+                        <Route path="/Historique" component={Hioistque}></Route>
+                        <Route path="detail/:id" component={DetailAnnonce}></Route>
+                        <Route path="/login" exact component={Login}></Route>
+                        <Route path="/login/:url" component={Login}></Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+         );
+    }
+}
+ 
+export default Navigation;
