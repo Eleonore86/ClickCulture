88b503f9457e97d1d4829d5041e93767bf2ff4b3
Rabah Khabzaoui <neuro@hotmail.fr>
2020-11-07 15:53:05 +0100

    creation composnt libraire

---
+++ react-culture/src/Component/Libraire.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { link } from 'react-router-dom';
+
+class Libraire extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state != nextState || this.props != nextProps
+    }
+    render() { 
+        const libraire = this.props.libraire
+        return ( 
+            <div className="row m-1 justify-content-center align-items-center">
+                 <div className="col-3">
+                     {/* Plusieur pics pour le libaire */}
+                     <img src={libraire.images[0]} />  
+                 </div>
+                 <div className="col-7">
+                     <div className="row m-1">
+                         <div className="col">
+                             {libraire.titre}
+                         </div>
+                     </div>
+                     <div className="row m-1">
+                         <div className="col">
+                             {libraire.description.substr(0, 50)}
+                         </div>
+                     </div>
+                 </div>
+                 <div className="col-2">
+                     <Link className="btn btn-primary" to={'/detail/' + lbraire.id}>Detail</Link>
+                 </div>
+             </div>
+         );
+    }
+}
+ 
+export default Libraire;
