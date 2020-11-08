fb9971718b6ca58f41023d0a39d725944237e3f5
Thomas.bausiere <mazo.zedif@gmail.com>
2020-11-08 17:16:19 +0100

    Home

--- react-culture/src/Component/Home.js
+++ react-culture/src/Component/Home.js
@@ -1,2 +1,59 @@
 import React, { Component } from 'react';
-import {libraires} from '../Service/DataService';
+import { libraires, getLibraires, search } from '../services/DataService';
+import Libraires from './libraires';
+// import Search from './Search';
+
+import {Spin, Input} from "antd"
+const {Search} = Input
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            loading : true,
+            libraires : []
+         }
+    }
+
+    componentDidMount() {
+        getLibraires().then((result) => {
+            this.setState({
+                loading : false,
+                Libraires : result.data.libraires
+            })
+        })
+    }
+
+    
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return this.state != nextState || this.props != nextProps
+    }
+
+    search = (text) => {
+        this.setState({
+            loading : true
+        })
+        search(text).then((result) => {
+            this.setState({
+                libraires : result.data.libraires,
+                loading : false
+            })
+        }).catch(err=> {
+            this.setState({
+                loading : false
+            })
+        }) 
+    }
+
+    render() { 
+        return ( 
+            <div className="container">
+                {/* <Search search={this.search}></Search> */}
+                <Search onChange={(e) => {this.search(e.target.value)}} placeholder="Votre recherche" enterButton="Search" size="large" loading={this.state.loading} />
+                {this.state.loading ? <div style={{textAlign: 'center'}}><Spin /></div> : <Libraires libraires={this.state.libraires}></Libraires>}
+                
+            </div>
+         );
+    }
+}
+ 
+export default Home;
